144892f27319dae2be6be80af3376cddfc7f5948
Gertjan Roggemans <gertjan.roggemans@collibra.com>
2019-02-17 22:11:36 +0100

    Fix provisioning_actions not being passed as list

--- main.tf
+++ main.tf
@@ -1,14 +1,24 @@
+locals {
+  create_vars = {
+    provisioning_actions = "${var.on_create_actions}"
+  }
+  destroy_vars = {
+    provisioning_order = "reverse"
+    provisioning_actions = "${var.on_destroy_actions}"
+  }
+}
+
 # Ansible roles-playbook
 resource "null_resource" "roles-playbook" {
   # Create
   provisioner "local-exec" {
-    command = "${join(" ", var.environment)} ansible-playbook ${path.module}/provisioning.yml -e 'provisioning_actions=${join(",", var.on_create_actions)}' -e '${jsonencode(var.variables)}' -i '${var.hosts},' ${join(" ", compact(var.arguments))}"
+    command = "${join(" ", var.environment)} ansible-playbook ${path.module}/provisioning.yml -e '${jsonencode(local.create_vars)}' -e '${jsonencode(var.variables)}' -i '${var.hosts},' ${join(" ", compact(var.arguments))}"
   }
 
   # Destroy
   provisioner "local-exec" {
     when       = "destroy"
-    command    = "${join(" ", var.environment)} ansible-playbook ${path.module}/provisioning.yml -e 'provisioning_order=reverse provisioning_actions=${join(",", var.on_destroy_actions)}' -e '${jsonencode(var.variables)}' -i '${var.hosts},' ${join(" ", compact(var.arguments))}"
+    command    = "${join(" ", var.environment)} ansible-playbook ${path.module}/provisioning.yml -e '${jsonencode(local.destroy_vars)}' -e '${jsonencode(var.variables)}' -i '${var.hosts},' ${join(" ", compact(var.arguments))}"
     on_failure = "continue"
     # @TODO: Update when #19679 is fixed
     #on_failure = "${var.on_destroy_failure}"
